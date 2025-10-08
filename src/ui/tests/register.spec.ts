7214c46ac9e24718033b442fb06ff2390708c400
Marat <margefter@gmail.com>
2025-10-08 08:25:03 +0300

    codestyle features implementation

--- src/ui/tests/register.spec.ts
+++ src/ui/tests/register.spec.ts
@@ -16,35 +16,35 @@ interface ICredentials {
 }
 
 test.describe("[https://anatoly-karpovich.github.io/demo-login-form/] [Form Registration]", () => {
-    
-    const validCredentials: ICredentials = {
-        username: "Vasya",
-        password: "Qwertyui"
-    }
+  const validCredentials: ICredentials = {
+    username: "Vasya",
+    password: "Qwertyu"
+  };
+  console.log();
 
-    enum NOTIFICATIONS {
-        REGISTER_SUCCESS = "Successfully registered! Please, click Back to return on login page"
-    }
+  enum NOTIFICATIONS {
+    REGISTER_SUCCESS = "Successfully registered! Please, click Back to return on login page"
+  }
 
-    test.beforeEach(async ({ page }) => {
-        const url = "https://anatoly-karpovich.github.io/demo-login-form/";
-        const registerButtonOnLogin = page.locator('#registerOnLogin');
-        const registerFormTitle = page.locator('#registerForm');
-        await page.goto(url);
-        await registerButtonOnLogin.click();
-        await expect(registerFormTitle).toBeVisible();
-    });
+  test.beforeEach(async ({ page }) => {
+    const url = "https://anatoly-karpovich.github.io/demo-login-form/";
+    const registerButtonOnLogin = page.locator("#registerOnLogin");
+    const registerFormTitle = page.locator("#registerForm");
+    await page.goto(url);
+    await registerButtonOnLogin.click();
+    await expect(registerFormTitle).toBeVisible();
+  });
 
-    test("Should be registered with valid credentials", async ({ page }) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButtonOnRegiser = page.locator("#passwordOnRegister");
-        const errorMessageOnRegister = page.locator("#errorMessageOnRegister");
+  test("Should be registered with valid credentials", async ({ page }) => {
+    const usernameInput = page.locator("#userNameOnRegister");
+    const passwordInput = page.locator("#passwordOnRegister");
+    const registerButtonOnRegiser = page.locator("#passwordOnRegister");
+    const errorMessageOnRegister = page.locator("#errorMessageOnRegister");
 
-        await usernameInput.fill(validCredentials.username);
-        await passwordInput.fill(validCredentials.password);
-        await registerButtonOnRegiser.click();
-        await expect(errorMessageOnRegister).toBeVisible();
-        await expect(errorMessageOnRegister).toContainText(NOTIFICATIONS.REGISTER_SUCCESS);
-    });
+    await usernameInput.fill(validCredentials.username);
+    await passwordInput.fill(validCredentials.password);
+    await registerButtonOnRegiser.click();
+    await expect(errorMessageOnRegister).toBeVisible();
+    await expect(errorMessageOnRegister).toContainText(NOTIFICATIONS.REGISTER_SUCCESS);
+  });
 });
