d93469b61a6c2763c9fbffc676040db6e30fa716
Marat <margefter@gmail.com>
2025-10-13 01:03:31 +0300

    2 task without local storage

--- src/ui/tests/login-form.spec.ts
+++ src/ui/tests/login-form.spec.ts
@@ -9,3 +9,37 @@
   - Для доступа к localStorage используйте https://playwright.dev/docs/evaluating
 
 */
+
+import test, { expect } from "@playwright/test";
+
+interface ICredentials {
+  username: string;
+  password: string;
+}
+
+test.describe("[https://anatoly-karpovich.github.io/demo-login-form/] [Form Auth]", () => {
+  const validCredentials: ICredentials = {
+    username: "test@gmail.com",
+    password: "SecretPw123!@#"
+  };
+
+  test.beforeEach(async ({ page }) => {
+    const url = "https://anatoly-karpovich.github.io/demo-login-form/";
+    const loginFormTitle = page.locator("#loginForm");
+    await page.goto(url);
+    await expect(loginFormTitle).toBeVisible();
+  });
+
+  test("Should be registered with valid credentials", async ({ page }) => {
+    const usernameInput = page.locator("#userName");
+    const passwordInput = page.locator("#password");
+    const registerButtonOnRegiser = page.locator("#submit");
+    const successMessage = page.locator("#successMessage");
+
+    await usernameInput.fill(validCredentials.username);
+    await passwordInput.fill(validCredentials.password);
+    await registerButtonOnRegiser.click();
+    await expect(successMessage).toBeVisible();
+    await expect(successMessage).toHaveText(`Hello, ${validCredentials.username}`);
+  });
+});
