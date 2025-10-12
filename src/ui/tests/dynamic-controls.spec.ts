16caa0ec67d15c9ceaf22fc0e17a52157a711f85
Marat <margefter@gmail.com>
2025-10-12 11:30:43 +0300

    1 task done

--- src/ui/tests/dynamic-controls.spec.ts
+++ src/ui/tests/dynamic-controls.spec.ts
@@ -14,16 +14,39 @@
   - Завалидировать текст It's back!
 */
 
-import test from "playwright/test";
+import test, { expect } from "playwright/test";
 
 test.describe("[https://the-internet.herokuapp.com] [Dynamic Controls]", () => {
   test("Dynamic controls page validation", async ({ page }) => {
     const url = "https://the-internet.herokuapp.com/";
-    const dynamicControlsLink = page.locator("//a[@href='/dynamic_controls']");
-    const removeButton = page.locator("#checkbox-example > button");
+    const dynamicControlsLink = page.getByRole("link", { name: "Dynamic Controls" });
+    const removeButton = page.getByRole("button", { name: "Remove" });
+    const dynamicControlsHeading = page.getByRole("heading", { name: "Dynamic Controls" });
+    const headingAdditionalText = page.locator("div.example > p");
+    const checkboxWithoutId = page.locator("div#checkbox input");
+    const checkboxWithId = page.locator("#checkbox");
+    const loader = page.locator("#loading");
+    const addButton = page.getByRole("button", { name: "Add" });
+    const message = page.locator("#message");
 
     await page.goto(url);
     await dynamicControlsLink.click();
     await removeButton.waitFor({ state: "visible" });
+    await expect(dynamicControlsHeading).toBeVisible();
+    await expect(headingAdditionalText).toHaveText(
+      "This example demonstrates when elements (e.g., checkbox, input field, etc.) are changed asynchronously."
+    );
+    await checkboxWithoutId.check();
+    await removeButton.click();
+    await expect(loader).toBeVisible();
+    await expect(loader).toBeHidden();
+    await expect(checkboxWithoutId).toBeHidden();
+    await addButton.waitFor({ state: "visible" });
+    await expect(message).toBeVisible();
+    await expect(message).toHaveText("It's gone!");
+    await addButton.click();
+    await expect(checkboxWithId).toBeVisible();
+    await expect(message).toBeVisible();
+    await expect(message).toHaveText("It's back!");
   });
 });
