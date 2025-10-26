5607047dea8a0bf0c274b01304df656155ee9cc4
Marat <margefter@gmail.com>
2025-10-26 11:08:58 +0300

    add e2e test

---
+++ src/tests/ui/sales-portal/products/add-new-product-homework.spec.ts
@@ -0,0 +1,35 @@
+import test, { expect } from "@playwright/test";
+import { credentials } from "config/env";
+import { NOTIFICATIONS } from "data/salesPortal/notifications";
+import { generateProductData } from "data/salesPortal/products/generateProductData";
+import { HomePage } from "ui/pages/home.page";
+import { AddNewProductPage } from "ui/pages/products/addNewProduct.page";
+import { ProductsListPage } from "ui/pages/products/productsList.page";
+import { SignInPage } from "ui/pages/signin.page";
+
+test.describe("[Sales Portal] [Products]", async () => {
+  test("Add new product e2e", async ({ page }) => {
+    const homePage = new HomePage(page);
+    const productsListPage = new ProductsListPage(page);
+    const addNewProductPage = new AddNewProductPage(page);
+    const signInPage = new SignInPage(page);
+
+    await homePage.open();
+
+    await signInPage.fillCredentials(credentials.username, credentials.password);
+    await signInPage.clickOnLoginButton();
+
+    await homePage.waitForOpened();
+    await homePage.clickOnViewModule("Products");
+    await productsListPage.waitForOpened();
+    await productsListPage.clickAddNewProduct();
+    await addNewProductPage.waitForOpened();
+    const productData = generateProductData();
+    await addNewProductPage.fillForm(productData);
+    await addNewProductPage.clickSave();
+    await productsListPage.waitForOpened();
+    await expect(productsListPage.toastMessage).toContainText(NOTIFICATIONS.PRODUCT_CREATED);
+    await expect(productsListPage.tableRowByName(productData.name)).toBeVisible();
+    await expect((await productsListPage.getProductTable).name);
+  });
+});
--- src/ui/pages/products/productsList.page.ts
+++ src/ui/pages/products/productsList.page.ts
@@ -1,3 +1,4 @@
+import { IProductTableRow } from "data/types/products-table-row";
 import { SalesPortalPage } from "../salesPortal.page";
 
 export class ProductsListPage extends SalesPortalPage {
@@ -10,5 +11,20 @@ export class ProductsListPage extends SalesPortalPage {
 
   async clickAddNewProduct() {
     await this.addNewProductButton.click();
+  }
+
+  async getProductTableRow(): Promise<IProductTableRow> {
+    const firstRow = this.page.locator("table tbody tr").first();
+    const cells = firstRow.locator("td");
+
+    const name = (await cells.nth(0).textContent())!;
+    const rawPrice = (await cells.nth(1).textContent())!;
+    const manufacturer = (await cells.nth(2).textContent())!;
+
+    return {
+      name,
+      price: `$${rawPrice.replace(/^\$/, "")}`,
+      manufacturer
+    };
   }
 }
