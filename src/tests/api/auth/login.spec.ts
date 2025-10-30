e3a3de4ee46d024cae6fcff656f03b725e187318
Marat <margefter@gmail.com>
2025-10-30 08:37:50 +0300

    task-1 done

--- src/tests/api/auth/login.spec.ts
+++ src/tests/api/auth/login.spec.ts
@@ -1,6 +1,6 @@
 import test, { expect } from "@playwright/test";
 import { apiConfig } from "config/apiConfig";
-import { credentials } from "config/env";
+import { adminCredentials } from "config/env";
 import { loginSchema } from "data/schemas/auth/login.schema";
 import { STATUS_CODES } from "data/statusCodes";
 import { validateResponse } from "utils/validateResponse.utils";
@@ -10,7 +10,7 @@ const { baseURL, endpoints } = apiConfig;
 test.describe("[API] [Sales Portal] [Login]", () => {
   test("Login", async ({ request }) => {
     const loginResponse = await request.post(baseURL + endpoints.login, {
-      data: credentials,
+      data: adminCredentials,
       headers: {
         "content-type": "application/json"
       }
