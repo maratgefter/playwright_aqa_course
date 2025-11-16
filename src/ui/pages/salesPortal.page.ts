875abe86bd35c4921dc08757f604b01127576f46
Marat <margefter@gmail.com>
2025-11-16 17:43:53 +0300

    hw-26 add code from lection and fix

--- src/ui/pages/salesPortal.page.ts
+++ src/ui/pages/salesPortal.page.ts
@@ -5,25 +5,23 @@ import { SALES_PORTAL_URL } from "config/env";
 export abstract class SalesPortalPage extends BasePage {
   readonly spinner = this.page.locator(".spinner-border");
   readonly toastMessage = this.page.locator(".toast-body");
-  readonly notificationCloseButton = this.page.locator("div.d-flex button[title = 'Close']");
+  readonly toastMwssageClose = this.page.locator("button[title='Close']");
   abstract readonly uniqueElement: Locator;
 
   async waitForOpened() {
-    await expect(this.uniqueElement).toBeVisible();
+    await expect(this.uniqueElement).toBeVisible({ timeout: 10000 });
     await this.waitForSpinners();
   }
 
   async waitForSpinners() {
-    await expect(this.spinner).toHaveCount(0);
+    await expect(this.spinner).toHaveCount(0, { timeout: 10000 });
   }
 
-  async open() {
-    console.log(SALES_PORTAL_URL);
-    await this.page.goto(SALES_PORTAL_URL);
+  async open(route?: string) {
+    await this.page.goto(SALES_PORTAL_URL + route);
   }
 
-  async notificationClose() {
-    await expect(this.notificationCloseButton).toBeVisible();
-    await this.notificationCloseButton.click();
+  async closeToastMessage() {
+    await this.toastMwssageClose.click();
   }
 }
--- src/ui/pages/signin.page.ts
+++ src/ui/pages/signin.page.ts
@@ -1,14 +1,15 @@
-import { BasePage } from "./base.page";
+import { ICredentials } from "data/types/credentials.types";
+import { SalesPortalPage } from "./salesPortal.page";
 
-export class SignInPage extends BasePage {
+export class SignInPage extends SalesPortalPage {
   readonly emailInput = this.page.locator("#emailinput");
   readonly passwordInput = this.page.locator("#passwordinput");
   readonly loginButton = this.page.locator("button[type = 'submit']");
   readonly uniqueElement = this.emailInput;
 
-  async fillCredentials(email: string, password: string) {
-    await this.emailInput.fill(email);
-    await this.passwordInput.fill(password);
+  async fillCredentials(credentials: Partial<ICredentials>) {
+    if (credentials.username) await this.emailInput.fill(credentials.username);
+    if (credentials.password) await this.passwordInput.fill(credentials.password);
   }
 
   async clickOnLoginButton() {
