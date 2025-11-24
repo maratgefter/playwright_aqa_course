aa7c7c13c53ac3cc592796577e5d03db448bc04f
Marat <margefter@gmail.com>
2025-11-24 13:08:50 +0300

    fixes

--- src/api/service/customers.service.ts
+++ src/api/service/customers.service.ts
@@ -5,8 +5,8 @@ import { validateResponse } from "utils/validation/validateResponse.utils";
 export class CustomersApiService {
   constructor(private customersApi: CustomersApi) {}
 
-  async delete(token: string, id: string) {
-    const response = await this.customersApi.delete(id, token);
+  async delete(token: string, _id: string) {
+    const response = await this.customersApi.delete(_id, token);
     validateResponse(response, {
       status: STATUS_CODES.DELETED
     });
--- src/tests/ui/sales-portal/customers/add-new-customer-e2e.spec.ts
+++ src/tests/ui/sales-portal/customers/add-new-customer-e2e.spec.ts
@@ -5,18 +5,12 @@ test.describe("[Sales Portal] [Customers]", () => {
   let id = "";
   let token = "";
 
-  test("Add new customer", async ({
-    loginUIService,
-    customersListPage,
-    customersListUIService,
-    addCustomerUIService
-  }) => {
+  test("Add new customer", async ({ loginUIService, customersListPage, addCustomerUIService, homePage }) => {
     token = await loginUIService.loginAsAdmin();
-    await customersListUIService.open();
-    await customersListUIService.openAddCustomerPage();
+    await homePage.clickOnViewModule("Customers");
+    await customersListPage.clickAddNewCustomer();
     const createdCustomer = await addCustomerUIService.create();
     id = createdCustomer._id;
-    await customersListUIService.open();
     await expect(customersListPage.toastMessage).toContainText(NOTIFICATIONS.CUSTOMER_CREATED);
     await expect(customersListPage.tableRowByName(createdCustomer.name)).toBeVisible();
   });
